5594c2c16ed6ca10219ed6c00c4e7f5102d70ee2
talesfromtheslums <nocapleanback@gmail.com>
2025-07-14 16:08:24 -0400

    Update ZenArchitecture.h

--- ZenArchitecture.h
+++ ZenArchitecture.h
@@ -1,4 +1,137 @@
+class Zen5CPU:
+    def __init__(self):
+        # Front End
+        self.l1_cache = L1Cache()
+        self.branch_predictor = BranchPredictor()
+        self.decoder = Decoder()
+        self.op_cache = OpCache()
+        self.uop_queue = UOPQueue()
 
+        # Back End
+        self.ns_queue = NSQueue()
+        self.vector_rename = VectorRename()
+        self.sch_list = [Scheduler() for _ in range(3)]  # 3 schedulers (assuming 3 pipelines)
+        self.general_purpose_regs = GPRegisters()
+        self.load_store_queues = LoadStoreQueues()
+
+        # Execution Units
+        self.alu_units = [ALU() for _ in range(6)]  # 6 ALUs
+        self.agus = [AGU() for _ in range(4)]       # 4 Address Generators
+        self.branch_units = [BranchUnit() for _ in range(4)]  # 4 Branch units
+        self.cache_units = CacheUnits()
+
+        # Memory Hierarchy
+        self.l1_cache = L1Cache()
+        self.l2_cache = L2Cache()
+
+        # Other components
+        self.memory_interface = MemoryInterface()
+        self.load_store_queues = LoadStoreQueues()
+        self.tlb = TLB()
+
+    def execute_cycle(self):
+        self.fetch()
+        self.decode()
+        self.execute()
+        self.write_back()
+
+    def fetch(self):
+        # Fetch instructions from L1 cache and branch predictor
+        pass
+
+    def decode(self):
+        # Decode instructions and prepare micro-operations
+        pass
+
+    def execute(self):
+        # Dispatch to execution units
+        pass
+
+    def write_back(self):
+        # Commit results to registers/memory
+        pass
+class L1Cache:
+    def __init__(self):
+        self.size = 32 * 1024  # 32KB
+        self.lines = 8  # 8-way associative
+        self.block_size = 64  # 64 bytes
+        # Additional properties...
+
+class L2Cache:
+    def __init__(self):
+        self.size = 1024 * 1024  # 1MB
+        self.lines = 16
+        self.block_size = 64
+
+class BranchPredictor:
+    def __init__(self):
+        pass
+
+    def predict(self, branch_address):
+        pass
+
+class Decoder:
+    def __init__(self):
+        pass
+
+    def decode_instruction(self, instruction_bytes):
+        pass
+
+class OpCache:
+    def __init__(self):
+        self.size = 6 * 1024  # 6KB
+        self.lines = 2  # 2-way associative
+        self.instructions = []
+
+class UOPQueue:
+    def __init__(self):
+        self.queue = []
+
+class NSQueue:
+    def __init__(self):
+        self.entries = 96
+
+class VectorRename:
+    def __init__(self):
+        pass
+
+class Scheduler:
+    def __init__(self):
+        # 3 schedulers
+        self.entries = 38
+
+class GPRegisters:
+    def __init__(self):
+        self.regs = [0] * 64  # 64 general purpose registers
+
+class LoadStoreQueues:
+    def __init__(self):
+        self.load_queue = []
+        self.store_queue = []
+
+class TLB:
+    def __init__(self):
+        self.entries = 64
+
+class ALU:
+    def __init__(self):
+        # ALU operation logic
+        pass
+
+class AGU:
+    def __init__(self):
+        # Address generation logic
+        pass
+
+class BranchUnit:
+    def __init__(self):
+        # Branch prediction and handling
+        pass
+
+class CacheUnits:
+    def __init__(self):
+        # Manage cache-related operations
+        pass
 class CPU:
     def __init__(self):
         self.registers = Registers()
